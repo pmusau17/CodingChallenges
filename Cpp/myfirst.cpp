76ec8b5e47cfe703c4483f7e5a010700e483a5e2
Patrick Musau <pmusau13ster@gmail.com>
2020-03-05 10:17:47 -0600

    beginning my journey of coding challenges in C++

---
+++ Cpp/myfirst.cpp
@@ -0,0 +1,11 @@
+//Learn the basics of C++
+
+#include <iostream> //A PREPROCESSOR directive
+
+int main() {  //Function header
+    using namespace std; //make definitions visible
+    cout << "Come up and C++ me some time."; //message
+    cout << endl; //start a new line
+    cout << "You won't regret it!" << endl; //more output
+    return 0; //terminate main
+}
