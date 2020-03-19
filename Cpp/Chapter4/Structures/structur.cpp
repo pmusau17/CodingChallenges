ec119cee6efdb3a02a19bb13c5b88a8205e5f978
Patrick Musau <pmusau13ster@gmail.com>
2020-03-18 20:07:52 -0500

    Introduction to structures

---
+++ Cpp/Chapter4/Structures/structur.cpp
@@ -0,0 +1,42 @@
+// structur.cpp -- a simple structure
+
+#include <iostream>
+
+struct inflatable  // structure declaration
+{
+    char name[20];
+    float volume;
+    double price;
+
+};
+
+int main()
+{
+    using namespace std;
+    inflatable guest =
+    {
+        "Glorious Gloria", // name
+        1.88,              // volume volume
+        29.99              // price value
+
+    }; // guest is a structure variable of type inflatable
+    // It's initialized to the indicated values
+
+    inflatable pal =
+    {
+        "Audacious Arthur",
+        3.12,
+        32.99
+    }; // pal is a second variable of type inflatable
+
+    // NOTE: some implementations require using 
+    // static inflatable guest = 
+
+    cout << "Expand your guest list with " << guest.name;
+    cout << " and " << pal.name << "!\n";
+
+    cout << "YOu can have both for $";
+    cout << guest.price + pal.price << "!\n";
+    return 0; 
+
+}
