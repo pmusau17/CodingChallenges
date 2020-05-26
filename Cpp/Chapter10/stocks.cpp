729ff4deb52b872585ffeff1d03e566b522527a2
Patrick Musau <pmusau13ster@gmail.com>
2020-05-26 11:56:40 -0500

    Finish Stocks Class

--- Cpp/Chapter10/stocks.cpp
+++ Cpp/Chapter10/stocks.cpp
@@ -12,9 +12,10 @@ class Stock // class declaration
         void set_tot() {total_val = shares * share_val;}
     
     public:
-        void acquire(const char * co; int n, double pr);
+        void acquire(const char * co, int n, double pr);
         void buy(int num, double price);
         void sell(int num, double price);
+        void update(double price);
         void show();
 
 }; // note the semicolone at the end
@@ -89,7 +90,29 @@ void Stock::show()
     using std::cout;
     using std::endl;
     cout << "Company: " << company
-         << " Shares: " << shares << endl;
-         << " Share Price: $" << share_val;
-         << " Total Worth: $" << total_val << endl; 
+         << ", Shares: " << shares << endl
+         << "Share Price: $" << share_val
+         << ", Total Worth: $" << total_val << endl; 
+}
+
+
+int main()
+{
+    using std::cout;
+    using std::ios_base;
+
+    Stock stock1;
+    stock1.acquire("NanoSmart",20,12.50);
+
+    cout.setf(ios_base::fixed); // #.## format
+    cout.precision(2);
+    cout.setf(ios_base::showpoint);
+
+    stock1.show();
+    stock1.buy(15,18.25);
+    stock1.show();
+    stock1.sell(400,20.0);
+    stock1.show();
+
+    return 0;
 }
