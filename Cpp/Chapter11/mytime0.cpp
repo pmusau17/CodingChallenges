fcc7e5cdd6d15057baad6fd1539ebfebbd44b71c
Patrick Musau <pmusau13ster@gmail.com>
2020-06-15 10:41:19 -0500

    Operator overloading

---
+++ Cpp/Chapter11/mytime0.cpp
@@ -0,0 +1,48 @@
+// mytime0.cpp -- implementing Time methods
+#include <iostream>
+#include "mytime0.h"
+
+// constructors
+Time::Time()
+{
+    this->hours=this->minutes=0;
+}
+
+Time::Time(int h, int m)
+{
+    this->hours = h;
+    this->minutes = m;
+}
+
+// member functions 
+void Time::AddMin(int m)
+{
+    this->minutes+=m;
+    this->hours +=this->minutes/60;
+    this->minutes &=60;
+}
+
+void Time::AddHr(int h)
+{
+    this->hours +=h;
+}
+
+void Time::Reset(int h, int m)
+{
+    this->hours =h;
+    this->minutes =m;
+}
+
+Time Time::Sum(const Time & t) const 
+{
+    Time sum;
+    sum.minutes = this->minutes + t.minutes;
+    sum.hours = this->hours +t.hours + sum.minutes/60;
+    sum.minutes %=60;
+    return sum; 
+}
+
+void Time::Show() const 
+{
+    std::cout << hours << " hours, " << minutes << " minutes";
+}
