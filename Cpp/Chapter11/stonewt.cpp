d7f6ab27d7798b3c764d62db567027480cc33edb
Patrick Musau <pmusau13ster@gmail.com>
2020-06-23 12:13:23 -0500

    Stones

---
+++ Cpp/Chapter11/stonewt.cpp
@@ -0,0 +1,41 @@
+// stonewt.cpp --Stonewt methods
+#include <iostream>
+using std::cout;
+#include "stonewt.h"
+
+// construct Stonewt object from double value
+Stonewt::Stonewt(double lbs)
+{
+    this->stone = int(lbs) / Lbs_per_stn; // integer division
+    this -> pds_left = int(lbs) % Lbs_per_stn + lbs - int(lbs); // the second part of this equation is the double remainder
+    this -> pounds = lbs;
+}
+
+// construct Stonewt object from stone double values
+Stonewt::Stonewt(int stn, double lbs)
+{
+    this->stone = stn;
+    this->pds_left = lbs;
+    this->pounds = stn * Lbs_per_stn + lbs;
+}
+
+Stonewt::Stonewt() // default constructor, wt =0 
+{
+    stone = pounds = pds_left =0;
+}
+
+Stonewt::~Stonewt() // destructor 
+{
+}
+
+// show weight in stones
+void Stonewt::show_stn() const
+{
+    cout << stone << " stone, " << pds_left << " pounds\n";
+}
+
+// show weight in pounds 
+void Stonewt::show_lbs() const
+{
+    cout << pounds << " pounds\n";
+}
