52ece6989c4bb9180999e0219ea0c8f42cd6d806
Patrick Musau <pmusau13ster@gmail.com>
2020-09-01 17:17:52 -0500

    read points into array

--- Cpp/MISC/test.c
+++ Cpp/MISC/test.c
@@ -1,10 +1,89 @@
 #include <stdio.h>
 #include <stdbool.h>
+#include <string.h> 
+#include <stdlib.h>
+
+
+
+
+
+
+
+void load_wallpoints(const char * filename);
+int countlines(const char * filename);
 
 
 // creds: https://www.geeksforgeeks.org/find-two-rectangles-overlap/
-
 bool check_safety(double (*rect)[2], double (*cone)[2]);
+
+
+
+
+int countlines(const char * filename)
+{
+    int cnt =0;
+    FILE *fp;
+    char line[60];
+
+    // open the file
+    fp = fopen(filename,"r");
+    if(fp==NULL)
+    {
+        printf("Could not open file %s",filename);
+    }
+    else
+    {
+        while( fgets(line, 60, fp)!=NULL )
+        {
+            cnt+=1;
+        }
+        fclose(fp);
+    }
+    return cnt;
+}
+
+void load_wallpoints(const char * filename)
+{
+    char line[60]; 
+    char * x; 
+    char * y;
+    double xd;
+    double yd;
+    FILE *wallPoints;
+    int count,i;
+    count = countlines(filename);
+    printf("Opening file...with %d points\n",count);
+
+    double wallCoords[count][2];
+
+    wallPoints = fopen(filename,"r");
+    if(wallPoints==NULL)
+    {
+        printf("Could not open file %s",filename);
+    }
+    else
+    {
+        i =0;
+        while( fgets (line, 60, wallPoints)!=NULL )
+        {
+            x = strtok(line,",");
+            y = strtok(NULL,",");
+            xd = strtod(x,NULL);
+            yd = strtod(y,NULL);
+            wallCoords[i][0] = xd;
+            wallCoords[i][1] = yd;
+            i+=1;
+
+        }
+        fclose(wallPoints);
+    }
+
+    for (int j=0;j< count;j++)
+    {
+        printf("%f,%f\n",wallCoords[j][0],wallCoords[j][1]);
+    }
+}
+
 
 bool check_safety(double (*rect)[2], double (*cone)[2])
 {
@@ -40,8 +119,17 @@ int main(void)
     double cone6[2][2] = {{1,2},{1,2}};
     double cone7[2][2] = {{1,2},{0,1}};
 
+    double cone8[2][2] = {{1.5,2.5},{1.5,2.5}};
+    double cone9[2][2] = {{2.0,2.0},{2.0,2.0}};
+
     bool intersect = check_safety(cone6,cone1);
     printf("overlap: %d\n",intersect);
+
     intersect = check_safety(cone7,cone1);
     printf("overlap: %d\n",intersect);
+
+    intersect = check_safety(cone8,cone9);
+    printf("overlap: %d\n",intersect);
+
+    load_wallpoints("porto_obstacles.txt");
 }
