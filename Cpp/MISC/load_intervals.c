b6426dd2d427ee70e5f08653f12d5bd89c97760e
Patrick Musau <pmusau13ster@gmail.com>
2020-09-18 09:39:16 -0500

    errors and obstacles

--- Cpp/MISC/load_intervals.c
+++ Cpp/MISC/load_intervals.c
@@ -11,33 +11,24 @@
 
 
 
-double points[5][2] = {{2.0,2.0},{4.7,2.7},{11.36,-1.46},{3.0,6.4},{-9.64,2.96}};
-double *** cones = 0;
+double points[6][2] = {{2.0,2.0},{4.7,2.7},{11.36,-1.46},{3.0,6.4},{-9.64,2.96},{2.0,2.0}};
+double *** obstacles = 0;
 
 
-
-/*double (*)[2] calculate_intervals(double center[2], double width,double height)
+// function that allocates the 3d array for the obstacles
+void allocate_obstacles(int num_obstacles,double (*points)[2])
 {
-    double interval[2][2] = {{center[0]-width/2.0,center[0]+width/2.0},{center[1]-height/2, center[1]+height/2}};
-    return interval;
-}*/
-
-
-// function that allocates the 3d array for the cones
-void allocate_cones(int num_cones,double (*points)[2])
-{
-
-    int rows = num_cones;
+    int rows = num_obstacles;
     int cols = 2;
     int height = 2;
     int i,j;
     double w = 0.13;
     double h = 0.13;
 
-    cones = (double***)malloc(rows * sizeof(double **));
+    obstacles = (double***)malloc(rows * sizeof(double **));
     
     // check if memory was allocated 
-    if(cones == NULL)
+    if(obstacles == NULL)
 	{
 		fprintf(stderr, "out of memory\n");
 		exit(0);
@@ -45,18 +36,18 @@ void allocate_cones(int num_cones,double (*points)[2])
 
     for(i=0;i<rows;i++)
     {
-        cones[i] = (double **)malloc(cols * sizeof(double*));
+        obstacles[i] = (double **)malloc(cols * sizeof(double*));
         // check if memory was allocated
-        if(cones[i] == NULL)
+        if(obstacles[i] == NULL)
 		{
 			fprintf(stderr, "out of memory\n");
 			exit(0);
 		}
         for(j=0;j<cols;j++)
         {
-            cones[i][j] = (double*)malloc(height*sizeof(double));
+            obstacles[i][j] = (double*)malloc(height*sizeof(double));
             // check if memory was allocated
-            if(cones[i][j] == NULL)
+            if(obstacles[i][j] == NULL)
 		    {
 			    fprintf(stderr, "out of memory\n");
 			    exit(0);
@@ -66,37 +57,33 @@ void allocate_cones(int num_cones,double (*points)[2])
 
     for(i=0;i<rows;i++)
     {
-        cones[i][0][0] = points[i][0]-w/2.0;
-        cones[i][0][1] = points[i][0]+w/2.0;
-        cones[i][1][0] = points[i][0]-h/2.0;
-        cones[i][1][1] = points[i][0]+h/2.0;
-        printf("[%f,%f], [%f,%f]\n", cones[i][0][0],cones[i][0][1],cones[i][1][0],cones[i][1][1]);
+        obstacles[i][0][0] = points[i][0]-w/2.0;
+        obstacles[i][0][1] = points[i][0]+w/2.0;
+        obstacles[i][1][0] = points[i][1]-h/2.0;
+        obstacles[i][1][1] = points[i][1]+h/2.0;
+        printf("[%f,%f], [%f,%f]\n", obstacles[i][0][0],obstacles[i][0][1],obstacles[i][1][0],obstacles[i][1][1]);
     }
 }
 
 // free the memory allocated for the wall points
-void deallocate_cones(int num_cones)
+void deallocate_obstacles(int num_obstacles)
 {
-    int rows = num_cones;
+    int rows = num_obstacles;
     int cols = 2;
-    int height = 2;
     int i,j;
     for(i=0;i<rows;i++)
     {
-        for(int j=0;j<cols;j++)
+        for(j=0;j<cols;j++)
         {
-            free(cones[i][j]);
+            free(obstacles[i][j]);
         }
-        free(cones[i]);
+        free(obstacles[i]);
     }
-    free(cones);
+    free(obstacles);
 }
 
 int main(void)
 {
-    //double cone[2][2]= calculate_intervals(points[0], 0.13, 0.13);
-    //printf("[%f,%f], [%f,%f]\n",cone[0][0],cone[0][1],cone[1][0],cone[1][1]);
-
-    allocate_cones(5,points);
-    deallocate_cones(5);
+    allocate_obstacles(6,points);
+    deallocate_obstacles(6);
 }
