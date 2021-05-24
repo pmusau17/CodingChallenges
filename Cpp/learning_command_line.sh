81176a85194038fe56d64d80fd4b30b6e8d1c40a
pmusau17 <pmusau13ster@gmail.com>
2021-05-24 17:24:24 -0500

    learning command line arguments

---
+++ Cpp/learning_command_line.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# These will be passed from the command line like so learning_command_line.sh john 25 'John Smith'
+echo "Username: $1";
+echo "Age: $2";
+echo "Full Name: $3";
---
+++ Cpp/learning_flags.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+while [[ $# -gt 0 ]]; do 
+    arg="$1"
+    echo $arg
+    case $arg in
+        -v|--version) 
+        echo "version tag"
+        ;;
+    esac
+    shift # moves to the next command line argument (this is dope)
+done 
